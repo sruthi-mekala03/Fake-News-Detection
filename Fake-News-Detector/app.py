adf0f29db70c5de5a821f9c577c1c21fecc4a86b
sruthi-mekala03 <mekalasruthi03@gmail.com>
2026-02-26 21:53:32 +0530

    Final update for deployment

--- Fake-News-Detector/app.py
+++ Fake-News-Detector/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import joblib
 import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
